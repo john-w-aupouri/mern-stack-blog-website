916608d716ae961d9867932cd9d534460da6a57a
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-06-01 18:00:54 +1200

    initial commit

---
+++ src/build/precache-manifest.988b082c57b32b426110a637ec450c02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b278c21d9dffbd56b0ebb013674f3c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c475aeb2680cfef8a81",
+    "url": "/static/css/main.9b60d65d.chunk.css"
+  },
+  {
+    "revision": "4f0cc2d2d5e699bfd719",
+    "url": "/static/js/2.ea36df12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ea36df12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c475aeb2680cfef8a81",
+    "url": "/static/js/main.9acb1159.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/static/media/expressjs-ar21.0fb30b49.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/static/media/mongodb-ar21.d571053a.svg"
+  },
+  {
+    "revision": "613cf57c00db5ba5c15b331620b6376f",
+    "url": "/static/media/nodejs-ar21.613cf57c.svg"
+  },
+  {
+    "revision": "ddd11c2b829e68ee21b9030bf3cc2727",
+    "url": "/static/media/reactjs-ar21.ddd11c2b.svg"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,107 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+// server
+const app = express();
+
+// middleware
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+// function to adhere to the DRY principal
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true});
+    const db = client.db('mern-stack-development');
+
+    await operations(db);
+
+    client.close();
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+}
+
+// get blog by name url parameter
+app.get('/api/blogs/:name', async (req, res) => {
+  withDB(async (db) => {
+    const blogName = req.params.name;
+
+    const blogInfo = await db.collection('blogs').findOne({ name: blogName });
+    res.status(200).json(blogInfo);
+  }, res);
+})
+
+// likes route
+app.post('/api/blogs/:name/likes', async (req, res) => {
+  withDB(async (db) => {
+    const blogName = req.params.name;
+
+    const blogInfo = await db.collection('blogs').findOne({ name: blogName });
+
+    // Querry to increment likes
+    await db.collection('blogs').updateOne({ name: blogName }, {
+      // mongodb syntax
+      '$set': {
+        likes: blogInfo.likes + 1,
+      }
+    });
+
+    // get updated blogInfo
+    const updatedBlogInfo = await db.collection('blogs').findOne({ name: blogName });
+
+    // send updatedBlogInfo back to the client
+    res.status(200).json(updatedBlogInfo);
+  }, res);
+});
+
+// dislikes route
+app.post('/api/blogs/:name/dislikes', async (req, res) => {
+  withDB(async (db) => {
+    const blogName = req.params.name;
+
+    const blogInfo = await db.collection('blogs').findOne({ name: blogName });
+    await db.collection('blogs').updateOne({ name: blogName }, {
+      '$set': {
+        dislikes: blogInfo.dislikes + 1,
+      }
+    });
+
+    // get updated blogInfo
+    const updatedBlogInfo = await db.collection('blogs').findOne({ name: blogName });
+
+    // send updatedBlogInfo back to the client
+    res.status(200).json(updatedBlogInfo);
+  }, res);
+  
+});
+
+// comments route
+// app.post('/api/blogs/:name/add-comment', (req, res) => {
+//   const { username, text } = req.body;
+//   const blogName = req.params.name;
+
+//   withDB(async (db) => {
+//     const blogInfo = await db.collection('blogs').findOne({ name: blogName });
+//       await db.collection('blogs').updateOne({ name: blogName }, {
+//         '$set': {
+//           comments: blogInfo.comments.concat({ username, text }),
+//         }
+//     });
+
+//     const updatedBlogInfo = await db.collection('blogs').findOne({ name: blogName });
+
+
+//     res.status(200).json(updatedBlogInfo);
+//   }, res);
+// });
+
+app.get('*', (req, res) => {
+  // all requests not caught by any other route should be passed onto App
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(8000, () => console.log('Listening on port 8000'));
